d2ec4df2d13320c247fb962bdd9f613ac95664ef
Joe Miragliuolo <sod@llawn.com>
2013-07-25 13:38:35 -0400

    add plaintext bookmarklet space for ios copying

--- bookmarklet.php
+++ bookmarklet.php
@@ -5,8 +5,13 @@ include(__DIR__."/nonpublicstuff/classes/template.class.php");
 
 $templates_dir    = "nonpublicstuff/templates/";
 
+$bookmarklet = new Template($GLOBALS['templates_dir']."bookmarklet.tpl.js");
+$bookmarklet->set('root_address', $config['root_address']);
+//echo $bookmarklet->output();
+
 $page = new Template($GLOBALS['templates_dir']."bookmarklet_page.tpl.html");
-$page->set('root_address', $config['root_address']);
+//$page->set('root_address', $config['root_address']);
+$page->set('bookmarklet', $bookmarklet->output());
 echo $page->output();
 
 ?>
---
+++ nonpublicstuff/templates/bookmarklet.tpl.js
@@ -0,0 +1,1 @@
+javascript:function jqrp1(){window.location.href='[@root_address]insert.php?post='+encodeURIComponent(document.location.href)+'&t='+(new Date().getTime())}jqrp1();void(0)
